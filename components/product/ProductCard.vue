aeb92d05279bb1f8b5636d7a10994dfdc5e6b002
haithanhphan1603 <haithanhphan163@gmail.com>
2024-08-20 20:45:28 +0700

    update product card wishlist

--- components/product/ProductCard.vue
+++ components/product/ProductCard.vue
@@ -10,6 +10,12 @@
       <Button v-if="isHovered" class="w-full font-extrabold absolute bottom-0">
         ADD TO CART
       </Button>
+      <button
+        class="absolute top-0 right-0 p-2 bg-transparent hover:bg-none"
+        @click="addToWishList"
+      >
+        <HeartIcon height="1.75rem" :color="heartIconColor" width="1.75rem" />
+      </button>
     </div>
     <CommonAppLink
       to="/"
@@ -31,9 +37,9 @@
 
 <script setup lang="ts">
 import Card from '../ui/card/Card.vue'
-import Button from '../ui/button/Button.vue'
 import AspectRatio from '../ui/aspect-ratio/AspectRatio.vue'
 import { useElementHover } from '@vueuse/core'
+import { HeartIcon } from 'lucide-vue-next'
 interface Props {
   product: Product
 }
@@ -42,6 +48,17 @@ defineProps<Props>()
 
 const myHoverableElement = ref<HTMLElement | null>(null)
 const isHovered = useElementHover(myHoverableElement)
+const isOnWishlist = ref(false)
+const colorMode = useColorMode()
+
+const heartIconColor = computed(() => {
+  const secondaryColor = colorMode.value === 'light' ? '#2d2d2d' : '#FFFFFF'
+  return isOnWishlist.value ? '#7c3aed' : secondaryColor
+})
+
+function addToWishList() {
+  isOnWishlist.value = !isOnWishlist.value
+}
 </script>
 
 <style scoped></style>
