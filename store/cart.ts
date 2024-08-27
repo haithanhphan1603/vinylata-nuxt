e8c04462ff8e0a2707195e3362fef37edc97ae03
haithanhphan1603 <haithanhphan163@gmail.com>
2024-08-27 23:13:57 +0700

    update cart

---
+++ store/cart.ts
@@ -0,0 +1,70 @@
+import { v4 as uuidv4 } from 'uuid'
+
+export const useCartStore = defineStore('cart', () => {
+  const cartItems = ref<CartItem[]>([])
+  const cart = ref<Cart | null>(null)
+
+  function createOrUpdateCart() {
+    if (!cart.value) {
+      cart.value = {
+        id: uuidv4(),
+        totalPrice: 0,
+        currency: 'USD',
+      }
+    }
+    cart.value.totalPrice = cartItems.value.reduce((acc, item) => {
+      return acc + item.price * item.quantity
+    }, 0)
+  }
+  function pushCartItem(item: CartItem) {
+    const currentCartItems = [...cartItems.value]
+    const existingItemIndex = currentCartItems.findIndex(
+      (i) => i.productId === item.productId,
+    )
+    if (existingItemIndex) {
+      currentCartItems[existingItemIndex].quantity += item.quantity
+    } else {
+      item.cartId = cart.value?.id || ''
+      currentCartItems.push(item)
+    }
+    cartItems.value = [...currentCartItems]
+    createOrUpdateCart()
+  }
+
+  function removeCartItem(productId: number) {
+    cartItems.value = cartItems.value.filter((i) => i.productId !== productId)
+    createOrUpdateCart()
+  }
+
+  function clearCart() {
+    cartItems.value = []
+    cart.value = null
+  }
+
+  function increaseItemQuantity(idx: number) {
+    const currentCartItems = [...cartItems.value]
+    currentCartItems[idx].quantity += 1
+    cartItems.value = [...currentCartItems]
+    createOrUpdateCart()
+  }
+
+  function decreaseItemQuantity(idx: number) {
+    const currentCartItems = [...cartItems.value]
+    if (currentCartItems[idx].quantity > 1) {
+      currentCartItems[idx].quantity -= 1
+    } else {
+      currentCartItems.splice(idx, 1)
+    }
+    cartItems.value = [...currentCartItems]
+    createOrUpdateCart()
+  }
+
+  return {
+    cartItems,
+    pushCartItem,
+    removeCartItem,
+    increaseItemQuantity,
+    clearCart,
+    decreaseItemQuantity,
+  }
+})
---
+++ types/cart.d.ts
@@ -0,0 +1,14 @@
+interface CartItem {
+  productId: number
+  quantity: number
+  cartId: string
+  price: number
+}
+
+interface Cart {
+  totalPrice: number
+  currency: string
+  createdBy?: string
+  id: string
+  currency: string
+}
