2705d3a6208323a3594924f59daf37d7f08fef8d
haithanhphan1603 <haithanhphan163@gmail.com>
2024-10-19 22:18:19 +0700

    update wishlist function

--- store/wishlist.ts
+++ store/wishlist.ts
@@ -1,5 +1,41 @@
-import { defineStore } from 'pinia'
-
+type WishlistItem = {
+  userId: string
+  productId: number
+}
 export const useMyWishlistStore = defineStore('wishlist', () => {
-  const wishlist = ref([])
+  const wishlist = ref<WishlistItem[]>([])
+  const user = useSupabaseUser()
+
+  const { addToWishlistApi, deleteWishlistItemApi, getWishlistItems } =
+    useApiServices()
+
+  function addToWishlist(productId: number) {
+    if (!user.value) return
+    const wishtListClone = [...wishlist.value]
+    wishtListClone.push({ userId: user.value.id, productId })
+    addToWishlistApi(user.value.id, productId)
+    wishlist.value = wishtListClone
+  }
+
+  function deleteWishlistItem(idx: number) {
+    if (!user.value) return
+    const wishtListClone = [...wishlist.value]
+    const productId = wishtListClone[idx].productId
+    wishtListClone.splice(idx, 1)
+    deleteWishlistItemApi(productId)
+  }
+
+  watch(user, async (newUser) => {
+    if (newUser) {
+      wishlist.value = await getWishlistItems(newUser.id)
+    } else {
+      wishlist.value = []
+    }
+  })
+
+  return {
+    wishlist,
+    addToWishlist,
+    deleteWishlistItem,
+  }
 })
